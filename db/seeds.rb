0f231bf10aba6b7be0ad48894a4f8ad09329ca08
Bengi Genç <genc.bengi@gmail.com>
2022-12-05 16:55:29 +0300

    Delete previously seeded data and add seeding message

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+puts "Seeding..."
+Aeroplane.destroy_all
 Aeroplane.create(name:"A-320", make_year:1972, country_of_origin: "USA", fuel_economy:"A+", long_range_cruise_speed: 0.7, passenger_capacity:100, crew:20, description:"Best plane ever!!!!!!!!!", img_url:"https://en.wikipedia.org/wiki/Airbus_A320_family#/media/File:Vistara_VT-TTF_at_Bengaluru,_2016-02-20.jpg")
+puts "Seeding done"
