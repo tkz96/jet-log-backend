6eb19930548c710ddf0af345ae5aac0509721be4
Bengi Genç <genc.bengi@gmail.com>
2022-12-05 16:19:50 +0300

    Add models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :reservations, dependent: :destroy
+  has_many :aeroplanes, through: :reservations
+
+  validates :name, presence: true
+  # validates :email, presence: true, uniqueness: true
 end
