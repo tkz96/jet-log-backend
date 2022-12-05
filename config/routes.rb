bf8185253fd80fc471014dd7852aee1d1c3a884d
Sharon Odhiambo <sharon.odhiambo100@gmail.com>
2022-12-05 16:32:50 +0300

    Update API endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :reservations
+      resources :aeroplanes
+      resources :users
+    end
+  end
   resources :reservations
   resources :aeroplanes
   resources :users
