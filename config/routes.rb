4991f9e37f449b1fe82e80ff7dba09d037e357c3
Berkay Akbas <berkayakbas@gmail.com>
2022-12-12 11:51:56 +0300

    Add role to the users table, Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
-Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :reservations
-      resources :aeroplanes
-      resources :users
-      post "/login", to: "users#login"
-      delete '/api/logout', to: 'sessions#destroy'
-    end
-  end
-  resources :reservations
-  resources :aeroplanes
-  resources :users
-
+Rails.application.routes.draw do	
+  namespace :api do	
+    namespace :v1 do	
+      resources :aeroplanes	
+      resources :users do	
+        resources :reservations	
+      end	
+      post "/login", to: "users#login"	
+      delete '/api/logout', to: 'sessions#destroy'	
+    end	
+  end	
+  resources :reservations	
+  resources :aeroplanes	
+  resources :users	
+	
 end
