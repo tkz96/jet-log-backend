6a1731eff4252f7b51514741bac36a07f47c329e
Bengi Genç <genc.bengi@gmail.com>
2022-12-15 18:40:52 +0300

    Remove commented old code

--- spec/requests/get_aeroplane_spec.rb
+++ spec/requests/get_aeroplane_spec.rb
@@ -7,10 +7,6 @@ RSpec.describe 'Aeroplanes', type: :request do
       get '/api/v1/aeroplanes'
     end
 
-    # it 'returns all aeroplanes' do
-    #   expect(json.size).to eq(10)
-    # end
-
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
     end
